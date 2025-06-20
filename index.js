d29f03fdf6ee667c283e97178a95e131cfd30add
kuang51221 <kuang51221@gmail.com>
2025-06-20 22:45:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import {
   getPublicKey,
   finalizeEvent,
   verifyEvent
-} from "https://esm.sh/@nostr/tools@2.15.0/pure?target=es2020";
+} from "https://jsr.io/@nostr/tools/pure@2.15.0";
 
-import { SimplePool } from "https://esm.sh/@nostr/tools@2.15.0/pool?target=es2020";
+import { SimplePool } from "https://jsr.io/@nostr/tools/pool@2.15.0";
 
 let sk, pk;
 const pool = new SimplePool();
